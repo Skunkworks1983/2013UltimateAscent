c3a747ed453fb62bf0caabe1d37577f75720682b
Owen Mattson <example4742@gmail.com>
2013-01-19 10:43:23 -0800

    Worked on Execute part in Drive Distance
    Added some if then else statements to stop at target distance

--- Mercury2013/Commands/DriveDistance.cpp
+++ Mercury2013/Commands/DriveDistance.cpp
@@ -27,21 +27,19 @@ void DriveDistance::Execute() {
 	 * If distance remaining is lesser than ___ inches, turn the motor off.  
 	 * When less than the inches desired, motor off until practically no power at full stop on target destination.
 	 */
-	
-	if(this->m_distanceDriven == this->m_targetDistance) 
-	
 	//Executes the distance command in order to go to the given distance.
 	float leftDist = CommandBase::driveBase->getLeftEncoder()->GetDistance();
 	//While driving the robot reads encoders in order to know how far it has traveled
 	float rightDist = CommandBase::driveBase->getRightEncoder()->GetDistance();
+	this->m_distanceDriven = (leftDist + rightDist) / 2;
+
+	if (this->m_distanceDriven >= this->m_targetDistance)
+		this->CommandBase::driveBase->setSpeed(0, 0);
+	else
+		this->CommandBase::driveBase->setSpeed(0.5, 0.5);
+
 	//Upon execution, the motors will continue to run until encoders realize DriveDistance=Finished
-	//	CommandBase::driveBase->SetSpeed(fmin(1, (distance - leftDist)/24), fmin(1,
-	//					(distance - rightDist)/24));
-	//Set motors to half power, let them go until you are within a certain distance.
-	//	if (fabs(leftDist - distance) < AUTO_DIST_THRESHOLD ||fabs(rightDist - distance) < AUTO_DIST
-	//TODO MAKE THIS UNBROKEN: REFER TO DRIVEDISTANCE.CPP in FRC UTILITIES
-	//if distanceTraveled<targetDistance turn motors on
-	//else turn motor off		
+
 }
 
 // Make this return true when this Command no longer needs to run execute()
