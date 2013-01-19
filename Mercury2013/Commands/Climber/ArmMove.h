33e2728aec469d0f2ccf9ddc4e87763afed4dc70
Trust Issues <skunk.trustissues@gmail.com>
2013-01-19 13:34:22 -0800

    Commiting again, same thing but fixed
    Its all good now, last change I made just being uploaded :D
    
    Signing Off: Connor Barlow

--- Mercury2013/Commands/Climber/ArmMove.h
+++ Mercury2013/Commands/Climber/ArmMove.h
@@ -1,8 +1,8 @@
 #ifndef __ARM_MOVE_H
 #define __ARM_MOVE_H
 #include "WPILib.h"
-#include "CommandBase.h"
-#include "../Subsystems/Climber.h"
+#include "../CommandBase.h"
+#include "../../Subsystems/Climber.h"
 
 class ArmMove: public CommandBase {
 public:
