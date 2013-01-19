0b3bf9f9875683bb8eb94fdb455be2fcb9181ee7
Owen Mattson <example4742@gmail.com>
2013-01-19 09:19:25 -0800

    Advances to Drive Distance with preseason code.
    Small area added into Drive Distance .cpp

--- Mercury2013/Commands/DriveDistance.cpp
+++ Mercury2013/Commands/DriveDistance.cpp
@@ -14,16 +14,23 @@ void DriveDistance::Initialize() {
 	CommandBase::driveBase->getLeftEncoder()->Reset();
 //	reset rightEncoder();	
 	CommandBase::driveBase->getRightEncoder()->Reset();
-	
+//putting all encoders to 0
+	this->count=0;
 	m_distanceDriven=0;
 }
 
 // Called repeatedly when this Command is scheduled to run
 void DriveDistance::Execute() {
 //Executes the distance command in order to go to the given distance.
+	float leftDist = CommandBase::driveBase->getLeftEncoder()->GetDistance();
 //While driving the robot reads encoders in order to know how far it has traveled
+	float rightDist = CommandBase::drivebase->GetRightEncoder()->GetDistance();
 //Upon execution, the motors will continue to run until encoders realize DriveDistance=Finished
+	CommandBase::driveBase->SetSpeed(fmin(1, (distance - leftDist)/24), fmin(1,
+					(distance - rightDist)/24));
 //Set motors to half power, let them go until you are within a certain distance.
+	if (fabs(leftDist - distance))
+		//TODO MAKE THIS UNBROKEN: REFER TO DRIVEDISTANCE.CPP in FRC UTILITIES
 //if distanceTraveled<targetDistance turn motors on
 //else turn motor off	
 	
