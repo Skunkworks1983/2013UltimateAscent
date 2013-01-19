a4429740f3456017504048f2ddaeec32be34ac92
Trust Issues <skunk.trustissues@gmail.com>
2013-01-19 13:34:22 -0800

    Commiting again, same thing but fixed
    Its all good now, last change I made just being uploaded :D
    
    Signing Off: Connor Barlow

---
+++ Mercury2013/Commands/Climber/ArmMove.cpp
@@ -0,0 +1,40 @@
+#include "ArmMove.h"
+
+ArmMove::ArmMove() {
+	
+}
+
+ArmMove::~ArmMove() {
+	
+}
+
+void ArmMove::Initialize() {
+	
+}
+
+void ArmMove::Execute() {
+	for (int i = 0; i < 2; i++) {
+		bool hooked = false;
+		while (hooked == false) {
+			climber->setArmsMotor(.25);
+			if (climber->getPosition() >= 100) { //TODO actuall number har
+				while ((climber->getSwitch(1) == false) and (climber->getSwitch(2) == false)) {
+					climber->setArmsMotor(-.25);
+					hooked = true;
+				}
+			}
+		}
+	}
+}
+
+bool ArmMove::IsFinished() {
+	return false;
+}
+
+void ArmMove::End() {
+	
+}
+
+void ArmMove::Interrupted() {
+	
+}
