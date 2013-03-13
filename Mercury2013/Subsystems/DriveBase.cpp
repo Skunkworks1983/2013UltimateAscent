efb61162250034ca973f1b9f5f159e7177d286a9
Bi-Polar Bear <example4742@gmail.com>
2013-03-12 18:59:34 -0700

    Encoders now shift too
    No more low-gear encoder troubles
    
    Signed off: Mark Old

--- Mercury2013/Subsystems/DriveBase.cpp
+++ Mercury2013/Subsystems/DriveBase.cpp
@@ -4,23 +4,13 @@
 DriveBase::DriveBase() :
 		Subsystem("DriveBase") {
 	printf("Creating drive base...\t");
-#ifdef DRIVE_MOTOR_LEFT_2		// Same thing ad infinitum
 	motorLeft = new DualLiveSpeed(new DRIVE_MOTOR_TYPE(DRIVE_MOTOR_LEFT),
 			new DRIVE_MOTOR_TYPE(DRIVE_MOTOR_LEFT_2));
-#else
-	motorLeft = new DualLiveSpeed(
-			motorLeft = new DRIVE_MOTOR_TYPE(DRIVE_MOTOR_LEFT),NULL);
-#endif
 	LiveWindow::GetInstance()->AddActuator("DriveBase", "Left Motor",
 			motorLeft);
 
-#ifdef DRIVE_MOTOR_RIGHT_2		// Same thing ad infinitum
 	motorRight = new DualLiveSpeed(new DRIVE_MOTOR_TYPE(DRIVE_MOTOR_RIGHT),
 			new DRIVE_MOTOR_TYPE(DRIVE_MOTOR_RIGHT_2));
-#else
-	motorRight = new DualLiveSpeed(
-			motorRight = new DRIVE_MOTOR_TYPE(DRIVE_MOTOR_RIGHT),NULL);
-#endif
 	LiveWindow::GetInstance()->AddActuator("DriveBase", "Right Motor",
 			motorRight);
 
@@ -28,29 +18,29 @@ DriveBase::DriveBase() :
 	LiveWindow::GetInstance()->AddActuator("DriveBase", "Shifter",
 			shiftSolenoid);
 
-#ifdef DRIVE_ENCODER_LEFT
 	leftEncoder = new Encoder(DRIVE_ENCODER_LEFT);
-	leftEncoder->SetDistancePerPulse(-DRIVE_ENCODER_INCH_PER_TICK);
+	if (isLowGear()) {
+		leftEncoder->SetDistancePerPulse(-DRIVE_ENCODER_INCH_PER_TICK_LOW);
+	} else {
+		leftEncoder->SetDistancePerPulse(-DRIVE_ENCODER_INCH_PER_TICK_HIGH);
+	}
 	leftEncoder->Start();
 	LiveWindow::GetInstance()->AddSensor("DriveBase", "LeftEncoder",
 			leftEncoder);
-#else
-	leftEncoder = NULL;
-#endif
-#ifdef DRIVE_ENCODER_RIGHT
+
 	rightEncoder = new Encoder(DRIVE_ENCODER_RIGHT);
-	rightEncoder->SetDistancePerPulse(DRIVE_ENCODER_INCH_PER_TICK);
+	if (isLowGear()) {
+		rightEncoder->SetDistancePerPulse(-DRIVE_ENCODER_INCH_PER_TICK_LOW);
+	} else {
+		rightEncoder->SetDistancePerPulse(-DRIVE_ENCODER_INCH_PER_TICK_HIGH);
+	}
 	rightEncoder->Start();
 	LiveWindow::GetInstance()->AddSensor("DriveBase", "RightEncoder",
 			rightEncoder);
-#else
-	rightEncoder = NULL;
-#endif
-
-#ifdef DRIVE_GYRO
+	
 	gyro = new Gyro(DRIVE_GYRO);
 	LiveWindow::GetInstance()->AddSensor("DriveBase", "Gyro", gyro);
-#endif
+	
 	printf("Done\n");
 }
 
@@ -58,15 +48,11 @@ DriveBase::~DriveBase() {
 	delete motorLeft;
 	delete motorRight;
 
-#ifdef DRIVE_ENCODER_LEFT
 	delete leftEncoder;
-#endif
-#ifdef DRIVE_ENCODER_RIGHT
 	delete rightEncoder;
-#endif
-#ifdef DRIVE_GYRO
+	
 	delete gyro;
-#endif
+	
 	delete shiftSolenoid;
 
 	LiveWindow::GetInstance()->AddSensor("DriveBase", "DriveGyro", gyro);
@@ -96,18 +82,20 @@ void DriveBase::InitDefaultCommand() {
 
 void DriveBase::shift(bool lowGear) {
 	shiftSolenoid->Set(lowGear);
+	if (lowGear) {
+		leftEncoder->SetDistancePerPulse(-DRIVE_ENCODER_INCH_PER_TICK_LOW);
+		rightEncoder->SetDistancePerPulse(-DRIVE_ENCODER_INCH_PER_TICK_LOW);
+	} else {
+		leftEncoder->SetDistancePerPulse(-DRIVE_ENCODER_INCH_PER_TICK_HIGH);
+		rightEncoder->SetDistancePerPulse(-DRIVE_ENCODER_INCH_PER_TICK_HIGH);
+	}
 }
 
 void DriveBase::reset() {
-#ifdef DRIVE_ENCODER_LEFT
 	leftEncoder->Reset();
-#endif
-#ifdef DRIVE_ENCODER_RIGHT
 	rightEncoder->Reset();
-#endif
-#ifdef DRIVE_GYRO
+
 	gyro->Reset();
-#endif
 }
 
 bool DriveBase::isLowGear() {
