e9346a89e2652fd4a06c16e5014415593695ed1b
Westin Miller <equinoxscripts@gmail.com>
2013-03-23 10:29:40 -0700

    Version Bump
    Bumped to version 1.2 -- Saturday, Ellensburg
    24 pt Auto and Teleop tuned

--- Mercury2013/UFOBot.cpp
+++ Mercury2013/UFOBot.cpp
@@ -86,9 +86,9 @@ void UFOBot::DisabledPeriodic() {
 
 void UFOBot::printVersion() {
 	DriverStationLCD::GetInstance()->Printf(DriverStationLCD::kUser_Line1, 1,
-			"Version 1.12");
+			"Version 1.2.0");
 	DriverStationLCD::GetInstance()->Printf(DriverStationLCD::kUser_Line2, 1,
-			"Thurs-Ellensburg");
+			"Sat-Begin-Ellensburg");
 	DriverStationLCD::GetInstance()->Printf(DriverStationLCD::kUser_Line3, 1,
 			"%s %s", __TIME__, __DATE__);
 	DriverStationLCD::GetInstance()->UpdateLCD();
