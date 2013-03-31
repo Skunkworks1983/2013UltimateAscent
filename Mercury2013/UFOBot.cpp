7a630ae00832b6b42f8ebc9270710666b7b8509e
Westin Miller <equinoxscripts@gmail.com>
2013-03-30 17:51:57 -0700

    v1.4 Seattle Final
    The final version from the Seattle regional.

--- Mercury2013/UFOBot.cpp
+++ Mercury2013/UFOBot.cpp
@@ -86,9 +86,9 @@ void UFOBot::DisabledPeriodic() {
 
 void UFOBot::printVersion() {
 	DriverStationLCD::GetInstance()->Printf(DriverStationLCD::kUser_Line1, 1,
-			"Version 1.3.2");
+			"Version 1.4");
 	DriverStationLCD::GetInstance()->Printf(DriverStationLCD::kUser_Line2, 1,
-			"Seattle-Quals-Begin");
+			"Seattle-Final");
 	DriverStationLCD::GetInstance()->Printf(DriverStationLCD::kUser_Line3, 1,
 			"%s %s", __TIME__, __DATE__);
 	DriverStationLCD::GetInstance()->UpdateLCD();
